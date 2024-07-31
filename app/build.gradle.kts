cd184f4efc718c9f90ee457d127ec11ccde4cf0d
Vishu Tyagi <tyagivishu277@gmail.com>
2024-07-31 17:32:34 +0530

    INITIAL COMMIT

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,9 +61,12 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.2.0")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
+  
     implementation("com.google.firebase:firebase-firestore:24.10.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation("com.google.firebase:firebase-auth")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/vishu/chatapp/LCViewModel.kt
+++ app/src/main/java/com/vishu/chatapp/LCViewModel.kt
@@ -1,5 +1,6 @@
 package com.vishu.chatapp
 
+import android.content.ContentValues.TAG
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
@@ -8,7 +9,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LCViewModel @Inject constructor(
-    val auth : FirebaseAuth
+   private val auth : FirebaseAuth
 
 ) : ViewModel() {
          init {
@@ -21,7 +22,9 @@ class LCViewModel @Inject constructor(
         auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
 
             if (it.isSuccessful){
-                Log.d("TAG", "signUp: User Logged IN ")
+                Log.d("TAG", "signInWithEmail:success")
+            }else{
+                Log.d("TAG", "signInWithEmail:failure", it.exception)
             }
 
         }
